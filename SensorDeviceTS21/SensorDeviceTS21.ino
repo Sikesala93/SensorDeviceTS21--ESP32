da3b7e91e77524387693d9d55a13bb90c2442e60
Tommi Uusitalo <tommi.uusitalo@edu.lapinamk.fi>
2021-11-13 23:47:32 +0200

    Update SensorDeviceTS21.ino

--- SensorDeviceTS21/SensorDeviceTS21.ino
+++ SensorDeviceTS21/SensorDeviceTS21.ino
@@ -1,10 +1,18 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+
 #include <pitches.h>
-#include <Tone32.h>
+#include <Tone32.h> //pitches.h ja Tone32.h -->  https://github.com/lbernstone/Tone32
  
 #define PIRPin 32
 #define SummeriPin 12
 #define LedPin 15
 #define BUZZ_CHAN 0
+
+#define SERVICE_UUID        "877a5e9a-74ae-4403-bfef-dc4c3fe2179f"
+#define CHARACTERISTIC_UUID "040983c3-bbcd-4311-b270-4e530359ad27"
 
 void setup() {
   Serial.begin(115200);
