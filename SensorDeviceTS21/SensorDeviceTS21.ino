8b72a41210e134ba49efd662dc4b8fa551fe719e
Tommi Uusitalo <tommi.uusitalo@edu.lapinamk.fi>
2021-11-14 00:35:14 +0200

    Connect ja disconnect morset

--- SensorDeviceTS21/SensorDeviceTS21.ino
+++ SensorDeviceTS21/SensorDeviceTS21.ino
@@ -11,8 +11,136 @@
 #define LedPin 15
 #define BUZZ_CHAN 0
 
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
 #define SERVICE_UUID        "877a5e9a-74ae-4403-bfef-dc4c3fe2179f"
 #define CHARACTERISTIC_UUID "040983c3-bbcd-4311-b270-4e530359ad27"
+// Lenght for morse space
+int morsePause = 90;
+bool deviceConnected = false;
+
+// Di ja dah morsetukseen
+void di(){
+  tone(SummeriPin, NOTE_G6, 30, BUZZ_CHAN);
+  noTone(SummeriPin, BUZZ_CHAN);
+}
+void dah(){
+  tone(SummeriPin, NOTE_G6, 90, BUZZ_CHAN);
+  noTone(SummeriPin, BUZZ_CHAN);
+}
+// connect ja disconnect morsetukset
+void onConnectMorse() {
+  // B
+  dah();
+  di();
+  di();
+  di();
+  delay(morsePause);
+  // T
+  dah();
+  delay(morsePause*2);
+  // O
+  dah();
+  dah();
+  dah();
+  delay(morsePause);
+  // K
+  dah();
+  di();
+  dah();
+  delay(morsePause*2);
+  
+}
+void onDisconnectMorse(){
+  // D
+  dah();
+  di();
+  di();
+  delay(morsePause);
+  // I
+  di();
+  di();
+  delay(morsePause);
+  // S 
+  di();
+  di();
+  di();
+  delay(morsePause);
+  // C
+  dah();
+  di();
+  dah();
+  di();
+  delay(morsePause);
+  // O 
+  dah();
+  dah();
+  dah();
+  delay(morsePause);
+  // N
+  dah();
+  di();
+  delay(morsePause);
+  // N
+  dah();
+  di();
+  delay(morsePause);
+  // E
+  di();
+  delay(morsePause);
+  // C
+  dah();
+  di();
+  dah();
+  di();
+  delay(morsePause);
+  // T
+  dah();
+  delay(morsePause);
+  // E
+  di();
+  delay(morsePause);
+  // D
+  dah();
+  di();
+  di();
+  delay(morsePause*2);
+  
+}
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("onConnect");
+      onConnectMorse();
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {   
+      Serial.println("onDisconnect");
+      onDisconnectMorse();
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+
+      if (value.length() > 0) {
+        Serial.println("*********");
+        Serial.print("New value: ");
+        for (int i = 0; i < value.length(); i++)
+          Serial.print(value[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+
+
+BLECharacteristic *pCharacteristic ;
 
 void setup() {
   Serial.begin(115200);
